00cae044ab8d117ad8ca607ac9b3c5dcd5628fe8
thomasmendez <thomasmendez01@gmail.com>
2023-02-23 00:52:42 -0600

    Updated bucket to block all access

--- terraform/development/create/bucket.tf
+++ terraform/development/create/bucket.tf
@@ -10,6 +10,15 @@ resource "aws_s3_bucket_acl" "bucketdev" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_public_access_block" "bucketdev" {
+  bucket = aws_s3_bucket.bucketdev.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_lifecycle_configuration" "bucketdev" {
   bucket = var.aws_bucket_name
   rule {
