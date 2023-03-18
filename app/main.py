ae94e842b79749bc24c916f1dfedd7824bf30042
thomasmendez <thomasmendez01@gmail.com>
2023-03-18 01:18:40 -0500

    Added .env for proxy routing

--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
 
 from app.api.api_v1.api import router as api_router
 from mangum import Mangum
 
+load_dotenv()
+env = os.getenv("ENV")
+
 app = FastAPI()
+
+if env != None and env != "local" :
+    print(env)
+    app.root_path = f"/{env}"
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-app.include_router(api_router, prefix="/api/v1")
+app.include_router(api_router)
+# handler = Mangum(app.include_router(APIRouter(prefix="/api/v1")))
 handler = Mangum(app)
